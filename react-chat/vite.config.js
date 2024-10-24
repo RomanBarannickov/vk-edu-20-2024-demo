976b65f866c54d5eecb912c805a34a7bb9eead00
Roman Barannikov <r.barannikov@vk.team>
2024-10-24 11:36:02 +0300

    fix deploy

--- react-chat/vite.config.js
+++ react-chat/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://romanbarannickov.github.io/vk-edu-20-2024-demo',
   build: {
     outDir: 'build', 
   },
